fa305a59dbefe915ac52ef8b53d910d0ee00d6ef
prasanthtr575 <mailprasanthtr@gmail.com>
2017-12-03 16:38:33 +0530

    Simple pagination added

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { debug } from 'util';
+import { denodeify } from 'q';
 
 @Component({
   selector: 'hn-list',
@@ -8,6 +9,9 @@ import { debug } from 'util';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  maxLimit: number = 5;
   hnIds: any[];
   hackerNews: any[] = [];
   url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -25,14 +29,44 @@ export class ListComponent implements OnInit {
   }
 
   fetchHackerNews() {
-    this.hnIds.map((id, index) => {
-      //Fetch News
-      let url = 'https://hacker-news.firebaseio.com/v0/item/' + id + '.json';
+    let page = this.getPageIndex(this.pageNumber);
+
+    this.hackerNews.length = 0;
+
+    for(let i = page.startIndex; i <= page.endIndex; i++) {
+      let newsId = this.hnIds[i];
+      let url = 'https://hacker-news.firebaseio.com/v0/item/' + newsId + '.json';
 
       this.http.get(url).subscribe(data => {
         // Read the result field from the JSON response.
         this.hackerNews.push(data);
+
+        window.scrollTo(0, document.body.clientHeight);
       });
-    });
+    }
+  }
+
+  getPageIndex(pageNumber) {
+    let startIndex = (this.pageNumber - 1) * 10; 
+    let endIndex = startIndex + this.pageSize - 1;
+
+    return {
+      startIndex,
+      endIndex
+    };
+  }
+
+  goToPreviousPage() {
+    if(this.pageNumber > 1) {
+      this.pageNumber--;
+      this.fetchHackerNews();
+    }
+  }
+
+  goToNextPage() {
+    if(this.pageNumber <= this.maxLimit) {
+      this.pageNumber++;
+      this.fetchHackerNews();
+    }
   }
 }
